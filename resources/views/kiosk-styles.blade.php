2e77d75f32eb3ce08fa7e1f853fc9fa2127ed8f8
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-03-15 00:58:23 +0800

    add cart and cancel and update home styles

--- resources/views/kiosk-styles.blade.php
+++ resources/views/kiosk-styles.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Styles -->
     <style>
@@ -58,7 +60,7 @@
             text-decoration: underline dotted
         }
 
-        h1,
+        /* h1,
         h2,
         h3,
         h4,
@@ -66,7 +68,7 @@
         h6 {
             font-size: inherit;
             font-weight: inherit
-        }
+        } */
 
         a {
             color: inherit;
@@ -131,14 +133,14 @@
             text-transform: none
         }
 
-        [type=button],
+        /* [type=button],
         [type=reset],
         [type=submit],
         button {
             -webkit-appearance: button;
             background-color: transparent;
             background-image: none
-        }
+        } */
 
         :-moz-focusring {
             outline: auto
@@ -856,12 +858,10 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
+
 
 </body>
 
