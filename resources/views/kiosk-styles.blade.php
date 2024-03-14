79b295af93aa5a9b3a3495c35c165f782aedfd36
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-03-14 22:31:10 +0800

    changed to EE-Lab

--- resources/views/kiosk-styles.blade.php
+++ resources/views/kiosk-styles.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>CODEE-Lab</title>
+    <title>EE-Lab</title>
     <link rel="apple-touch-icon" sizes="180x180" href="/images/bsu-neu-logo.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/bsu-neu-logo.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/bsu-neu-logo.png">
