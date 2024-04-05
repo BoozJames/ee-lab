a66d5acc00237983c51f5376cf4225f5f3151f30
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-04-05 18:46:54 +0800

    remove login register, dashboard button

--- resources/views/kiosk.blade.php
+++ resources/views/kiosk.blade.php
@@ -4,7 +4,7 @@
         <div class="background">
             <div
                 class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center selection:bg-red-500 selection:text-white">
-                @if (Route::has('login'))
+                {{-- @if (Route::has('login'))
                     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                         @auth
                             <a href="{{ url('/dashboard') }}"
@@ -14,13 +14,13 @@
                                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
                                 in</a>
 
-                            {{-- @if (Route::has('register'))
+                            @if (Route::has('register'))
                             <a href="{{ route('register') }}"
                                 class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif --}}
+                        @endif
                         @endauth
                     </div>
-                @endif
+                @endif --}}
 
                 <div class="max-w-7xl mx-auto p-6 lg:p-8">
                     <div class="flex justify-center">
