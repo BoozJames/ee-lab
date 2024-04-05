415d7ad36212f80aae3f4a03cf701d3395dc1146
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-04-05 18:47:00 +0800

    update navbar

--- resources/views/create-request-form.blade.php
+++ resources/views/create-request-form.blade.php
@@ -5,7 +5,7 @@
 
             <nav class="navbar navbar-expand-lg osition fixed-top bg-danger">
                 <div class="container-fluid">
-                    <a class="navbar-brand text-white" href="/">EE-Lab</a>
+                    {{-- <a class="navbar-brand text-white" href="/">EE-Lab</a> --}}
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
--- resources/views/requestors.blade.php
+++ resources/views/requestors.blade.php
@@ -4,11 +4,13 @@
 
     <nav class="navbar navbar-expand-lg osition fixed-top bg-danger">
         <div class="container-fluid">
-            <a class="navbar-brand text-white" href="/">EE-Lab</a>
+            {{-- <a class="navbar-brand text-white" href="/">EE-Lab</a> --}}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            </div>
             <button type="button" class="btn btn-secondary position-relative d-flex mx-1 my-1"
                 onclick="cancelAndRemoveCart()">
                 Cancel
