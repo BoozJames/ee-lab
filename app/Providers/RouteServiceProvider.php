66df92af61bba62813745912442a91d610cf34df
James Booz Manumbale <jamesboozmanumbale@gmail.com>
2024-06-19 23:46:16 +0800

    if login success go to /requests and update nav

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/requests';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
