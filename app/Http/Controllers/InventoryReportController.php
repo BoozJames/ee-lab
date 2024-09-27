a2decc0b74c780e0503f03604fe0c51ab3bad024
qwertymelvin <melvin.asa@gmail.com>
2024-09-27 08:24:50 +0800

    Fix form validation

--- app/Http/Controllers/InventoryReportController.php
+++ app/Http/Controllers/InventoryReportController.php
@@ -62,7 +62,12 @@ class InventoryReportController extends Controller
         ]);
 
         // Fetch data from the ItemVariants table and associate it with the newly created InventoryReport
-        $itemVariants = ItemVariants::where('status', '!=', 'Condemned')->get();
+        // $itemVariants = ItemVariants::where('status', '!=', 'Condemned')->get();
+        $itemVariants = ItemVariants::where('status', '!=', 'Condemned')
+                ->whereHas('item', function ($query) {
+                    $query->whereNull('deleted_at'); // Assuming soft deletes are used
+                })
+                ->get();
 
         // Create inventory report items
         foreach ($itemVariants as $itemVariant) {
